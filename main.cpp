ef648859c1516b13b17f823e4371eacf051fb1cd
Isabel Cristina <isawithans@gmail.com>
2024-09-23 21:19:59 -0300

    deu erro em um caso

--- main.cpp
+++ main.cpp
@@ -120,7 +120,7 @@ class OrderedFile {
       return low;
     }
 
-    void redistribute(int begin, int end, Data item) {
+    void redistribute(int begin, int end) {
       int n = this->list.get_n(begin, end);
       int blank_space = (end - begin + 1) - n;
       int steps = ceil((double) blank_space / (n - 1));
@@ -139,11 +139,9 @@ class OrderedFile {
       int pos = begin;
       for(int i = 0; i < j; i++) {
         this->list.values[pos] = items[i];
-        pos += steps + 1;
+        if(pos + steps + 1 > end) { pos += 1; }
+        else { pos += steps + 1; } 
       }
-
-      this->list.print();
-      this->list.print();
     }
 
   public:
@@ -173,7 +171,7 @@ class OrderedFile {
 
       int size = end - begin + 1;
       int element_count = this->list.get_n(begin, end) + 1;
-      int leaf_idx = begin / this->list.get_range();
+      int leaf_idx = begin / range;
       int range_increase = 1;
       
       double density = this->list.get_density(begin, end, element_count);
@@ -191,13 +189,22 @@ class OrderedFile {
       }
 
       while(out_bounds && !((end_total - begin_total + 1) == this->list.size)) {
-        if (leaf_idx & 1) {
+        int leaf_size = ceil((double)size/range);
+        if(size < range * leaf_size) {
           end = begin - 1;
-          begin = begin - range_increase * this->list.get_range();
+          int b = begin - range_increase * 2 * range;
+          begin = b < 0 ? 0 : b;
+          depth--;
+        }
+        else if (leaf_idx & 1) {
+          end = begin - 1;
+          begin = begin - range_increase * range;
         }
         else {
-          begin   =  end + 1;  
-          end =  end + range_increase * this->list.get_range();
+          begin = end + 1;  
+          end = end + range_increase * range;
+          if(end > this->list.size - 1) 
+            end = this->list.size - 1;
         }
 
         range_increase *= 2;
@@ -210,20 +217,24 @@ class OrderedFile {
         density = ((double) element_count / size); 
         depth--;
 
-        pair<double, double> bound = this->list.get_density_bound(depth);
+        bound = this->list.get_density_bound(depth);
         out_bounds =  density > bound.second || density < bound.first;
 
         if (((end_total - begin_total + 1) == this->list.size) && out_bounds) {
           this->list.change_size(this->list.size * 2);
+          break;
         }
       }
 
-      if(this->list.values[pos].value > x) 
+      if (this->list.values[pos].is_null) 
+        this->list.values[pos] = item;
+      else if(this->list.values[pos].value > x) 
         this->list.shift_right(pos);
       else 
         this->list.shift_left(pos);
       this->list.values[pos] = item;
-      redistribute(begin_total, end_total, item);
+
+      redistribute(begin_total, end_total);
     }
 
     void remove(int x){
@@ -236,24 +247,206 @@ class OrderedFile {
 int main() {
   OrderedFile file;
 
-  file.insert(8);
-  file.print();
-  file.insert(7);
-  file.print();
-  file.insert(6);
-  file.print();
-  file.insert(5);
-  file.print();
-  file.insert(4);
-  file.print();
-  file.insert(6);
-  file.print();
-  file.insert(7);
-  file.print();
-  file.insert(8);
-  file.print();
-  file.insert(9);
-  file.print();
+file.insert(145);
+file.print();
+file.insert(108);
+file.print();
+file.insert(162);
+file.print();
+file.insert(189);
+file.print();
+file.insert(136);
+file.print();
+file.insert(151);
+file.print();
+file.insert(172);
+file.print();
+file.insert(110);
+file.print();
+file.insert(196);
+file.print();
+file.insert(130);
+file.print();
+file.insert(177);
+file.print();
+file.insert(122);
+file.print();
+file.insert(185);
+file.print();
+file.insert(133);
+file.print();
+file.insert(101);
+file.print();
+file.insert(198);
+file.print();
+file.insert(154);
+file.print();
+file.insert(123);
+file.print();
+file.insert(139);
+file.print();
+file.insert(147);
+file.print();
+file.insert(113);
+file.print();
+file.insert(103);
+file.print();
+file.insert(194);
+file.print();
+file.insert(159);
+file.print();
+file.insert(179);
+file.print();
+file.insert(165);
+file.print();
+file.insert(173);
+file.print();
+file.insert(199);
+file.print();
+file.insert(109);
+file.print();
+file.insert(141);
+file.print();
+file.insert(132);
+file.print();
+file.insert(114);
+file.print();
+file.insert(160);
+file.print();
+file.insert(111);
+file.print();
+file.insert(193);
+file.print();
+file.insert(107);
+file.print();
+file.insert(169);
+file.print();
+file.insert(155);
+file.print();
+file.insert(175);
+file.print();
+file.insert(116);
+file.print();
+file.insert(187);
+file.print();
+file.insert(125);
+file.print();
+file.insert(144);
+file.print();
+file.insert(153);
+file.print();
+file.insert(120);
+file.print();
+file.insert(140);
+file.print();
+file.insert(167);
+file.print();
+file.insert(135);
+file.print();
+file.insert(190);
+file.print();
+file.insert(128);
+file.print();
+file.insert(118);
+file.print();
+file.insert(176);
+file.print();
+file.insert(104);
+file.print();
+file.insert(102);
+file.print();
+file.insert(121);
+file.print();
+file.insert(142);
+file.print();
+file.insert(157);
+file.print();
+file.insert(126);
+file.print();
+file.insert(192);
+file.print();
+file.insert(148);
+file.print();
+file.insert(138);
+file.print();
+file.insert(112);
+file.print();
+file.insert(149);
+file.print();
+file.insert(124);
+file.print();
+file.insert(158);
+file.print();
+file.insert(106);
+file.print();
+file.insert(161);
+file.print();
+file.insert(129);
+file.print();
+file.insert(131);
+file.print();
+file.insert(156);
+file.print();
+file.insert(117);
+file.print();
+file.insert(163);
+file.print();
+file.insert(186);
+file.print();
+file.insert(152);
+file.print();
+file.insert(119);
+file.print();
+file.insert(143);
+file.print();
+file.insert(137);
+file.print();
+file.insert(166);
+file.print();
+file.insert(134);
+file.print();
+file.insert(115);
+file.print();
+file.insert(150);
+file.print();
+file.insert(200);
+file.print();
+file.insert(105);
+file.print();
+file.insert(191);
+file.print();
+file.insert(164);
+file.print();
+file.insert(146);
+file.print();
+file.insert(170);
+file.print();
+file.insert(188);
+file.print();
+file.insert(171);
+file.print();
+file.insert(197);
+file.print();
+file.insert(168);
+file.print();
+file.insert(180);
+file.print();
+file.insert(127);
+file.print();
+file.insert(184);
+file.print();
+file.insert(181);
+file.print();
+file.insert(178);
+file.print();
+file.insert(182);
+file.print();
+file.insert(174);
+file.print();
+file.insert(183);
+file.print();
+file.insert(100);
+file.print();
 
   return 0;
 }
